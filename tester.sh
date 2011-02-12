576ea69d68f2477129b43cf67be91f324e718066
Kurt Smith <kwmsmith@gmail.com>
2011-02-11 18:14:30 -0600

    moved field_trace.py  and modified tester.sh script

--- tester.sh
+++ tester.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-(cd test && ./nosetests -v -s test_gsl_interp2d.py test_trace_integrator.py)
+(cd test && ./nosetests -v -s test_gsl_interp2d.py test_trace_integrator.py test_detect_nulls.py)
