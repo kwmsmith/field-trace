bcf1eaac1d6bdae29a553f52159589a9c1503da5
Kurt Smith <kwmsmith@gmail.com>
2011-03-16 14:23:01 -0500

    contour_tree(), first working version

--- contour_tree.py
+++ contour_tree.py
@@ -15,26 +15,27 @@ def uf_merge(uf_map, key1, key2):
     for key in keys_to_change:
         uf_map[key] = updated_set
 
-def join_split_tree(mesh, height_func):
+def join_split_tree(mesh, height_func, join_split_fac=1.0):
     join_tree = nx.DiGraph()
     # map of nodes to union-find set that it's in
     uf_map = {}
     # map of union-find set id to lowest node in the set.
     lowest_node_map = {}
     # a list of (height, node) tuples
-    height_and_nodes = [(height_func(node), node) for node in mesh.nodes()]
+    height_and_nodes = [(join_split_fac * height_func(node), node) for node in mesh.nodes()]
     height_and_nodes.sort(reverse=True)
     for h, n in height_and_nodes:
         uf_map[n] = set([n])
         lowest_node_map[id(uf_map[n])] = n
         for nbr in mesh.neighbors_iter(n):
-            nbr_h = height_func(nbr)
+            nbr_h = join_split_fac * height_func(nbr)
             if nbr_h < h or uf_map[nbr] is uf_map[n]:
                 continue
             # uf_merge() can change the uf_map[nbr] id value. Store it
             # for later retrieval.
             uf_map_nbr_id = id(uf_map[nbr])
             uf_merge(uf_map, n, nbr)
+            assert join_split_fac * height_func(lowest_node_map[uf_map_nbr_id]) > h
             join_tree.add_edge(lowest_node_map[uf_map_nbr_id], n)
             lowest_node_map[id(uf_map[nbr])] = n
     return join_tree
@@ -46,4 +47,56 @@ def peak_pit_nodes(join):
 def pass_nodes(join):
     in_deg = join.in_degree()
     out_deg = join.out_degree()
-    return [n for n in in_deg if in_deg[n] == 2 and out_deg[n] == 1]
+    return [n for n in in_deg if in_deg[n] >= 2 and out_deg[n] == 1]
+
+def is_leaf(node, join, split):
+    return is_upper_leaf(node, join, split) or is_lower_leaf(node, join, split)
+
+def is_upper_leaf(leaf, join, split):
+    return not join.in_degree(leaf) and split.in_degree(leaf) == 1
+
+def is_lower_leaf(leaf, join, split):
+    return not split.in_degree(leaf) and join.in_degree(leaf) == 1
+
+def reduce_graph(graph, node):
+    preds = graph.predecessors(node)
+    succs = graph.successors(node)
+    if len(preds) not in (0, 1):
+        import pdb; pdb.set_trace()
+    if len(succs) not in (0, 1):
+        import pdb; pdb.set_trace()
+    if preds:
+        assert len(preds) == 1
+        if succs:
+            assert len(succs) == 1
+            graph.add_edge(preds[0], succs[0])
+        graph.remove_edge(preds[0], node)
+    elif succs:
+        assert len(succs) == 1
+        graph.remove_edge(node, succs[0])
+    graph.remove_node(node)
+
+def contour_tree(mesh, height_func):
+    join = join_split_tree(mesh, height_func, join_split_fac=1.0)
+    join.name = 'join'
+    split = join_split_tree(mesh, height_func, join_split_fac=-1.0)
+    split.name = 'split'
+    c_tree = nx.Graph()
+    leaves = peak_pit_nodes(join) + peak_pit_nodes(split)
+    while len(leaves) > 1:
+        leaf = leaves.pop()
+        if is_upper_leaf(leaf, join, split):
+            this = join
+            other = split
+        else:
+            this = split
+            other = join
+        this_succ = this.successors(leaf)
+        assert len(this_succ) == 1
+        nbr = this_succ[0]
+        c_tree.add_edge(leaf, nbr)
+        reduce_graph(this, leaf)
+        reduce_graph(other, leaf)
+        if is_leaf(nbr, join, split):
+            leaves.append(nbr)
+    return c_tree
--- test/test_contour_tree.py
+++ test/test_contour_tree.py
@@ -5,28 +5,84 @@ import contour_tree as ct
 
 from nose.tools import eq_, ok_
 
-def test_example():
-    mesh = nx.Graph()
-    mesh.add_edges_from(mesh_edges)
-    eq_(len(mesh), 18)
-    height_func = lambda n: n
-    join = ct.join_split_tree(mesh, height_func)
-    neg_height_func = lambda n: -n
-    split = ct.join_split_tree(mesh, neg_height_func)
-    eq_(join.order(), split.order())
-    eq_(sorted(join.nodes()), sorted(split.nodes()))
-    eq_(sorted(ct.peak_pit_nodes(join)), [7, 8, 9, 10])
-    eq_(sorted(ct.peak_pit_nodes(split)), [1, 2])
-    eq_(sorted(ct.pass_nodes(join)), [4, 5, 6])
-    eq_(sorted(ct.pass_nodes(split)), [3])
-    import pylab as pl
-    pl.ion()
-    nx.draw_spring(join)
-    pl.title('join')
-    pl.figure()
-    nx.draw_spring(split)
-    pl.title('split')
-    raw_input("enter to continue")
+class test_contour_tree(object):
+
+    def setUp(self):
+        self.mesh = nx.Graph()
+        self.mesh.add_edges_from(mesh_edges)
+        self.height_func = lambda n: n
+
+    def test_join_split_trees(self):
+        eq_(len(self.mesh), 18)
+        join = ct.join_split_tree(self.mesh, self.height_func)
+        split = ct.join_split_tree(self.mesh, self.height_func, join_split_fac=-1.0)
+        eq_(join.order(), split.order())
+        eq_(sorted(join.nodes()), sorted(split.nodes()))
+        eq_(sorted(ct.peak_pit_nodes(join)), [7, 8, 9, 10])
+        eq_(sorted(ct.peak_pit_nodes(split)), [1, 2])
+        eq_(sorted(ct.pass_nodes(join)), [4, 5, 6])
+        eq_(sorted(ct.pass_nodes(split)), [3])
+
+        eq_(join.adj,
+                {
+                    1: {},
+                    2: {1: {}},
+                    2.1: {2: {}},
+                    3: {2.1: {}},
+                    4: {3: {}},
+                    4.6: {4: {}},
+                    4.9: {4: {}},
+                    5: {4.6: {}},
+                    6: {4.9: {}},
+                    6.1: {5: {}},
+                    6.5: {5: {}},
+                    6.9: {6: {}},
+                    7: {6: {}},
+                    7.2: {6.1: {}},
+                    8: {6.9: {}},
+                    8.3: {7.2: {}},
+                    9: {6.5: {}},
+                    10: {8.3: {}},
+                    }
+                )
+
+        eq_(split.adj,
+                {
+                    1: {2.1: {}},
+                    2: {3: {}},
+                    2.1: {3: {}},
+                    3: {4: {}},
+                    4: {4.6: {}},
+                    4.6: {4.9: {}},
+                    4.9: {5: {}},
+                    5: {6: {}},
+                    6: {6.1: {}},
+                    6.1: {6.5: {}},
+                    6.5: {6.9: {}},
+                    6.9: {7: {}},
+                    7: {7.2: {}},
+                    7.2: {8: {}},
+                    8: {8.3: {}},
+                    8.3: {9: {}},
+                    9: {10: {}},
+                    10: {}})
+
+        if 1:
+            import pylab as pl
+            pl.ion()
+            nx.draw_shell(join)
+            pl.title('join')
+            pl.figure()
+            nx.draw_shell(split)
+            pl.title('split')
+            raw_input("enter to continue")
+
+    def test_contour_tree(self):
+        contour_tree = ct.contour_tree(self.mesh, self.height_func)
+        import pylab as pl
+        pl.ion()
+        nx.draw(contour_tree)
+        raw_input("enter to continue")
 
 mesh_edges = [
         (1, 2.1),
