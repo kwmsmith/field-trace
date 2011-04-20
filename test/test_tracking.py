e62f9792209c9bae8503117ece0098920f59e3a2
Kurt Smith <kwmsmith@gmail.com>
2011-04-20 09:39:37 -0500

    updates to tracking & test_tracking

--- test/test_tracking.py
+++ test/test_tracking.py
@@ -1,9 +1,101 @@
+from os import path
+import tables
 import tracking
+from tracking import TrackRegion
 import numpy as np
+import _critical_points as _cp
+from pickle import load, dump
+
+from contour_tree import wraparound_dist
+
+import pylab as pl
 
 from nose.tools import ok_, eq_
 
-class test_track(object):
+def surf_networks(h5fname, force=False):
+    saved_name = path.realpath(h5fname)+'_pickled_surf_networks'
+    if not force:
+        try:
+            fh = open(saved_name, 'rb')
+        except IOError:
+            pass
+        else:
+            snets = load(fh)
+            fh.close()
+            return snets
+    # rebuild the surface networks
+    dta = tables.openFile(h5fname, 'r')
+    psigp = dta.getNode('/psi')._v_children
+    snets = []
+    for psi_arr_name in sorted(psigp)[:1]:
+        psi_arr = psigp[psi_arr_name].read()
+        surf = _cp.TopoSurface(psi_arr)
+        snets.append(surf)
+    with open(saved_name, 'w') as fh:
+        dump(snets, fh)
+    return snets
+
+def extract_regions(h5fname, force=False):
+    snets = surf_networks(h5fname, force=force)
+    regions = []
+    for snet in snets:
+        psi_regions = snet.get_minmax_regions()
+        tslice = []
+        for (cpt, type) in psi_regions:
+            area = len(psi_regions[cpt, type])
+            minmax = snet.arr[cpt]
+            tslice.append(TrackRegion(loc=cpt, area=area, val=minmax, ispeak=(type=='peak')))
+        regions.append(tslice)
+    return regions
+
+def track_regions_image(slice_pts, h5fname):
+    all_regions = extract_regions('data.h5')
+    dta = tables.openFile(h5fname, 'r')
+    psigp = dta.getNode('/psi')._v_children
+    pl.ioff()
+    for idx, psi_arr_name in enumerate(sorted(psigp)):
+        print idx
+        psi_arr = psigp[psi_arr_name].read()
+        pl.figure()
+        pl.imshow(psi_arr, interpolation='nearest', cmap='hot')
+        for (loc, num) in slice_pts[idx]:
+            pl.text(loc[1], loc[0], str(num))
+        xs = [reg.loc[0] for reg in all_regions[idx]]
+        ys = [reg.loc[1] for reg in all_regions[idx]]
+        pl.scatter(ys, xs, c='r', marker='s')
+        pl.savefig('psi_%03d.pdf' % idx)
+        pl.close('all')
+    dta.close()
+
+def test_track_greedy():
+    nx, ny = 512, 512
+    wdist = wraparound_dist(nx, ny)
+    all_regions = extract_regions('data.h5')
+    tracks = tracking.track_regions_greedy(all_regions, nx, ny)
+    pl.ion()
+    # val_fig = pl.figure()
+    # area_fig = pl.figure()
+    # psn_fig = pl.figure()
+    maxlen = len(all_regions)
+    tracks = [tr for tr in tracks if len(tr) >= 0.5 * maxlen]
+    from collections import defaultdict
+    slice_pts = defaultdict(list)
+    for num, tr in enumerate(tracks):
+        for idx, reg in tr:
+            slice_pts[idx].append((reg.loc, num))
+    track_regions_image(slice_pts, 'data.h5')
+    # for tr in tracks:
+        # idxs, regs = zip(*tr)
+        # pl.figure(val_fig.number)
+        # pl.plot(idxs, [reg.val for reg in regs], 's-', hold=True)
+        # pl.figure(area_fig.number)
+        # pl.semilogy(idxs, [reg.area for reg in regs], 's-', hold=True)
+        # pl.figure(psn_fig.number)
+        # dists = [wdist(regs[i].loc, regs[i+1].loc) for i in range(len(regs)-1)]
+        # pl.plot(idxs[:-1], dists, 's-', hold=True)
+    # import pdb; pdb.set_trace()
+
+class _test_track(object):
 
     def setUp(self):
         self.npts = 500
--- tracking.py
+++ tracking.py
@@ -1,21 +1,28 @@
 from anneal_discrete import Annealer
-from contour_tree import wraparound_dist
+from contour_tree import wraparound_dist_vec, wraparound_dist
 import numpy as np
 
+from scipy.spatial import cKDTree
+
 def get_dist_map(regions_0, regions_1, nx, ny):
-    wdist = wraparound_dist(nx, ny)
+    wdist = wraparound_dist_vec(nx, ny)
     dist_map = np.empty((len(regions_0), len(regions_1)), dtype=np.float_)
+    x0, y0 = regions_0[:,0], regions_0[:,1]
+    x1, y1 = regions_1[:,0], regions_1[:,1]
     for i, region0 in enumerate(regions_0):
-        for j, region1 in enumerate(regions_1):
-            dist_map[i, j] = wdist(region0, region1)
+        dist_map[i, :] = wdist(region0[0], region0[1], x1, y1)
     return dist_map
 
-dist_weight = 10.0
-area_weight = 1.0
+dist_weight = 1.0
+area_weight = 0.0
+val_weight = 0.0
 
 def fitness(reg0, reg1, dist):
-    area_diff = abs(reg1[2] - reg0[2])
-    return dist_weight * dist + area_weight * area_diff
+    if reg0.ispeak != reg1.ispeak: return np.inf
+    r1area, r0area = reg1.area, reg0.area
+    area_diff = abs(r1area - r0area)
+    val_diff = abs(reg1.val - reg0.val)
+    return dist_weight * dist**3 + area_weight * area_diff + val_diff * val_weight
 
 def get_sim_map(_fitness, dist_map, regions_0, regions_1):
     sim_map = list()
@@ -29,7 +36,7 @@ def sort_sim_map(sim_map):
     for sm in sim_map:
         sm.sort(key=lambda x: x[1])
 
-def track_forward_backward(regions_0, regions_1, nx, ny):
+def _track_forward_backward(regions_0, regions_1, nx, ny):
     dist_map_forward = get_dist_map(regions_0, regions_1, nx, ny)
     dist_map_backward = dist_map_forward.T
     sim_map_forward = get_sim_map(fitness, dist_map_forward, regions_0, regions_1)
@@ -52,8 +59,90 @@ def track_forward_backward(regions_0, regions_1, nx, ny):
 
     import pdb; pdb.set_trace()
 
+class KDTwraparound(object):
 
-def track_regions(regions_0, regions_1, nx, ny, nnbrs=5):
+    def __init__(self, pts, nx, ny):
+        self.nx, self.ny = nx, ny
+        self.all_pts = np.vstack([
+            pts+[-nx,-ny], pts+[-nx,0], pts+[-nx,+ny],
+            pts+[  0,-ny], pts        , pts+[  0,+ny],
+            pts+[+nx,-ny], pts+[+nx,0], pts+[+nx,+ny],
+            ])
+        self.kdt = cKDTree(self.all_pts)
+
+    def query(self, pts, k):
+        d, i = self.kdt.query(pts, k=k, p=2)
+        return d, (self.all_pts[i] % [self.nx, self.ny])
+
+def normalize_regions(all_regions, maxdiff):
+    maxdiff = float(maxdiff)
+    regions = all_regions[-1]
+    max_area = max([r.area for r in regions])
+    min_area = min([r.area for r in regions])
+    areadiff = np.abs(max_area - min_area)
+    area_fac = maxdiff / areadiff
+    max_val = max([r.val for r in regions])
+    min_val = min([r.val for r in regions])
+    valdiff = np.abs(max_val - min_val)
+    val_fac = maxdiff / valdiff
+    for regions in all_regions:
+        for r in regions:
+            r.area *= area_fac
+            r.val *= val_fac
+
+class TrackRegion(object):
+
+    def __init__(self, loc, area, val, ispeak):
+        self.loc = tuple(loc)
+        self.area = area
+        self.val = val
+        self.ispeak = ispeak
+
+    def __eq__(self, other):
+        return (self.loc == other.loc and
+                self.area == other.area and
+                self.val == other.val and
+                self.ispeak == other.ispeak)
+
+    def __hash__(self):
+        return hash(self.loc) + hash(self.area) + hash(self.val) + hash(self.ispeak)
+
+def track_regions_greedy(all_regions, nx, ny):
+    wdist = wraparound_dist(nx, ny)
+    normalize_regions(all_regions, nx/2)
+    r0s = all_regions[0]
+    # area_cutoff = np.median(np.array([r.area for r in r0s]))
+    area_cutoff = 0.5
+    regions0 = all_regions[0]
+    regions0 = sorted(regions0, key=lambda x: x.area)
+    regions0 = [r for r in regions0 if r.area > area_cutoff]
+    tslice_maps = []
+    for idx in range(len(all_regions)-1):
+        regions1 = all_regions[idx+1]
+        regions1 = sorted(regions1, key=lambda x: x.area)
+        regions1 = [r for r in regions1 if r.area > area_cutoff]
+        r0_to_r1 = {}
+        for region0 in regions0:
+            best_r1 = min([(fitness(region0, region1, wdist(region0.loc, region1.loc)), region1)
+                for region1 in regions1])[1]
+            r0_to_r1[region0] = best_r1
+        tslice_maps.append(r0_to_r1)
+        regions0 = regions1
+    # make tracks out of the tslice_maps list.
+    tracks = []
+    for idx, map in enumerate(tslice_maps):
+        # handle those that are in ``tracks``
+        for track in tracks:
+            if track[-1][1] in map:
+                reg = track[-1][1]
+                track.append((idx+1, map[reg]))
+                del map[reg]
+        # start a new track for everything else
+        for reg in map:
+            tracks.append([(idx, reg), (idx+1, map[reg])])
+    return tracks
+
+def track_regions_anneal(regions_0, regions_1, nx, ny, nnbrs=5):
 
     dist_map = get_dist_map(regions_0, regions_1, nx, ny)
 
