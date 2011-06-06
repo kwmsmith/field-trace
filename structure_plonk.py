e95a8cf6a64454e3123c28741cdfa21d6501770d
Kurt Smith <kwmsmith@gmail.com>
2011-06-06 13:49:55 -0500

    finished version of structure_plonk and tests

--- structure_plonk.py
+++ structure_plonk.py
@@ -1,5 +1,8 @@
+import pylab as pl
 import numpy as np
 from mag_shear import cartesian_to_polar_coords
+from scipy.optimize import leastsq
+from scipy.stats import kurtosis
 
 def structure_plonk(size, nstruts, amp, rad_exp, core_radius=5, seed=None):
     arr = np.zeros((size, size), dtype=np.double)
@@ -18,4 +21,55 @@ def structure_plonk(size, nstruts, amp, rad_exp, core_radius=5, seed=None):
         struct *= (sign * amp / np.max(struct))
         struct[dist<core_radius] = sign * amp
         arr += struct
+    arr -= arr.mean()
     return arr
+
+def hists(arrs, labels, title, savename):
+    nbins = np.sqrt(arrs[0].size)
+    pl.figure()
+    fits = [gaus_fit(np.histogram(arr, normed=True, bins=nbins), arr.mean(), arr.std()) \
+            for arr in arrs]
+    (ns, bins, patches) = pl.hist(x=arrs, histtype='step', label=labels, log=True, bins=100, normed=True, linewidth=3)
+    ymin, ymax = pl.ylim()
+    pl.ylim(1e-4, ymax)
+    for fit in fits:
+        best_x ,best_sig = fit
+        pl.plot(bins, gaussian(bins, best_x, best_sig), linewidth=3)
+    pl.legend()
+    pl.grid()
+    pl.xlabel('Amplitude (norm. units)')
+    pl.ylabel('PDF')
+    pl.title(title)
+    pl.savefig(savename)
+
+def all_hists(exponent, savename, title, seed=1, repeat=4, size=512):
+    np.random.seed(seed)
+    exponents = (6, 9)
+    arrs = []
+    for exp in exponents:
+        concat_arrs = []
+        for _ in range(repeat):
+            concat_arrs.append(structure_plonk(size=size, nstruts=2**exp, amp=1.0, rad_exp=exponent, core_radius=2))
+        arrs.append(np.concatenate(concat_arrs).flatten())
+    labels = [r'{0:d} strctures, $\kappa={1:3.1f}$'.format(2**i, kurtosis(arr)) for i, arr in zip(exponents, arrs)]
+    hists(arrs, labels, title, savename=savename)
+
+def gaussian(Xs, x0, sig):
+    norm = 1. / (sig * np.sqrt(2 * np.pi))
+    return norm * np.exp(-(Xs-x0)**2 / (2. * sig**2))
+
+def gaus_fit(dta_hist, x0, sig0):
+    def resid_func(y):
+        Y, X = dta_hist
+        x0, sig = y
+        expected = gaussian(X[:-1], x0, sig)
+        return Y - expected
+    y0 = np.array([x0, sig0])
+    result, success = leastsq(resid_func, y0)
+    if success not in range(1,5):
+        raise RuntimeError("result not found")
+    return result
+
+if __name__ == '__main__':
+    all_hists(exponent=1, repeat=8, savename='rad-exp-1-sim.pdf', title=r'Simulated $r^{-1}$ density structure PDFs')
+    all_hists(exponent=2, repeat=8, savename='rad-exp-2-sim.pdf', title=r'Simulated $r^{-2}$ density structure PDFs')
--- test/test_structure_plonk.py
+++ test/test_structure_plonk.py
@@ -1,9 +1,23 @@
-from field_trace.structure_plonk import structure_plonk
+import numpy as np
+from numpy import random
+from field_trace.structure_plonk import structure_plonk, gaus_fit, gaussian
 
 import pylab as pl
 
-def test_structure_plonk():
+def _test_structure_plonk():
     arr = structure_plonk(size=512, nstruts=40, amp=2.0, rad_exp=3, core_radius=5, seed=1)
     pl.ion()
     pl.imshow(arr, interpolation='nearest', cmap='hot')
+    raw_input('enter to continue')
+
+def test_leastsq():
+    pl.ion()
+    dta = random.normal(loc=2.0, scale=3.0, size=10000)
+    dta_hist = np.histogram(dta, normed=True, bins=np.sqrt(dta.size))
+    x0, sig0 = dta.mean(), dta.std()
+    best_x, best_sig = gaus_fit(dta_hist, x0, sig0)
+    pl.figure()
+    n, bins, patches = pl.hist(dta, normed=True, bins=np.sqrt(dta.size), histtype='step')
+    fit = gaussian(bins, best_x, best_sig)
+    pl.plot(bins, fit)
     raw_input('enter to continue')
